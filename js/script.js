bad3bb723a2e9a5107c03915abbd55a2e46857bf
ViniRivaldar <vinicius-rivaldar@outlook.com>
2023-11-27 15:33:49 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,8 +1,55 @@
-import DisplayToday from "./modules/displayToday.js";
-import TodoList from "./modules/todoList.js";
+class DisplayToday {
+    constructor(element) {
+      this.element = document.querySelector(element);
+      this.date = new Date();
+      this.options = { day: "numeric", month: "numeric" };
+    }
+  
+    updateElement() {
+      this.element.innerText = new Intl.DateTimeFormat(
+        "pt-br",
+        this.options,
+      ).format(this.date);
+    }
+  
+    init() {
+      if (this.element) this.updateElement();
+      return this;
+    }
+}
 
 const displayToday = new DisplayToday(".today");
 displayToday.init();
 
-const todoList = new TodoList(".create-button", ".todo-list");
-todoList.init();
+function addTask() {
+    let todoList = document.querySelector("ul");
+
+    if (!todoList) {
+        todoList = document.createElement("ul");
+        document.body.appendChild(todoList); 
+        todoList.children.length +1
+    }
+
+    let novaTodoList = document.createElement("ul");
+
+    novaTodoList.innerHTML = `
+        <ul class="todo-list">
+             <li class="task">
+                <input type="checkbox">
+                <textarea name="" id="" cols="100" rows="2"></textarea>
+                <button class="delete-button" onclick="deleteTask()"><i class="bi bi-trash3-fill"></i></button>
+             </li>
+        </ul>
+    `;
+
+    // Adicionar o novo li à lista
+    todoList.appendChild(novaTodoList);
+}
+
+
+function deleteTask() {
+    let todoList = document.querySelector("ul");
+    if (todoList.children.length > 0) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
